e36e65cf90be1d1e7922cbe9f2ba49e79446c577
Ansony Huallpa Mirano <ansonyhuallpam@gmail.com>
2023-09-23 18:10:49 -0500

    appComponent:agregando incrementador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title:string  = 'hola mundo';
+  public counter:number = 10;
+  increaseBy(valor:number):void {
+    this.counter+=valor;
+  }
+  resetCounter(){
+    this.counter=10;
+  }
 }
